b09d07881ad6c4a79979ad426209f568412604b8
ashmagill <ashmagill@developersinstitute.co.nz>
2023-10-24 12:09:49 +1300

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";
+import CategoriesScreen from "./screens/CategoriesScreen";
+
+export default function App() {
+  return <CategoriesScreen />;
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
